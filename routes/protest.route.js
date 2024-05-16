8012a930f5f519e20780b71c12270eb7ef9e0d50
victoralcan <victoralcan@gmail.com>
2024-05-16 18:49:29 -0300

    :recycle: Refact protest and user routes to use new errors

--- routes/protest.route.js
+++ routes/protest.route.js
@@ -5,39 +5,39 @@ const router = express.Router();
 const prisma = require('@prisma/client');
 const protestService = new ProtestService(new prisma.PrismaClient());
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const protest = await protestService.addProtest(req.body);
     res.status(201).json(protest);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    next(error);
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const protest = await protestService.getProtestById(req.params.id);
     res.status(200).json(protest);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    next(error);
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', async (req, res, next) => {
   try {
     const protest = await protestService.updateProtest(req.params.id, req.body);
     res.status(200).json(protest);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    next(error);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
   try {
     await protestService.deleteProtest(req.params.id);
     res.status(204).send();
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    next(error);
   }
 });
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,39 +5,39 @@ const router = express.Router();
 const prisma = require('@prisma/client');
 const userService = new UserService(new prisma.PrismaClient());
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const user = await userService.addUser(req.body);
     res.status(201).json(user);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    next(error);
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const user = await userService.getUserById(req.params.id);
     res.status(200).json(user);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    next(error);
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', async (req, res, next) => {
   try {
     const user = await userService.updateUser(req.params.id, req.body);
     res.status(200).json(user);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    next(error);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
   try {
     await userService.deleteUser(req.params.id);
     res.status(204).send();
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    next(error);
   }
 });
 
