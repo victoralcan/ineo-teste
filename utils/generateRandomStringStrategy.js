1ee534612a1fa6c355d0e40648fbd18857921a13
victoralcan <victoralcan@gmail.com>
2024-05-16 21:22:27 -0300

    :sparkles: Add generate random string util

---
+++ utils/generateRandomStringStrategy.js
@@ -0,0 +1,7 @@
+const crypto = require('crypto');
+
+function generateRandomString(length) {
+  return crypto.randomBytes(length).toString('hex').slice(0, length);
+}
+
+module.exports = generateRandomString;
